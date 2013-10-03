7aeb5d3287769eba3fbb5565be6859d5ae8fc2f2
seespotfly <adamvhill@gmail.com>
2013-10-02 21:26:37 -0400

    Specs Passing (hopefully)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 JamesApp2::Application.routes.draw do
-  devise_scope :user do; root to: 'devise/sessions#new'; end
+  get "welcome/index"
+  root to: "welcome#index"
+
+  resources :events
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
